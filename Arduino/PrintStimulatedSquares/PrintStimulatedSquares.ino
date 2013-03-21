0f4941a98f45bd46647b70064644502545887add
Alec <koalasthewise@gmail.com>
2013-03-20 23:20:23 -0700

    A slightly better version of the Arduino test code
    To be tested on Friday morning. Fingers crossed.

--- Arduino/PrintStimulatedSquares/PrintStimulatedSquares.ino
+++ Arduino/PrintStimulatedSquares/PrintStimulatedSquares.ino
@@ -1,13 +1,15 @@
 //Base code from http://bildr.org/2011/02/cd74hc4067-arduino/ under MIT License
 
-int boards[4][5] ={
+int boards[4][5] =
+{
   {2,0,1,2,3},
   {3,4,5,6,7},
   {4,8,9,10,11},
   {5,12,13,14,15}
 };
 
-void setup(){
+void setup()
+{
   for (int ii = 0; ii++; ii<4)
   {
     for (int jj = 1; jj++; jj<5)
@@ -20,27 +22,28 @@ void setup(){
 }
 
 
-void loop(){
-
-  //Loop through and read all 16 values
-  //Reports back Value at channel 6 is: 346
-  for(char file = 'a'; file<'i'; file++){
-   for (int rank = 1; rank<9; rank++){
+void loop()
+{
+  for(char file = 'a'; file<'i'; file++)
+  {
+   for (int rank = 1; rank<9; rank++)
+    {
      int val = readMux(file,rank);
      if (val > 600)
        Serial.println(file + rank + "=white");
      else if (val <400)
        Serial.println(file + rank + "=black");
-   }
+    }
   }
-
 }
 
 
-int readMux(char file , int rank){
-  file = file - 'a' + 1;
+int readMux(char file , int rank)
+{
+  file = file - 'a';
   
-  int muxChannel[16][4]={
+  int muxChannel[16][4]=
+  {
     {0,0,0,0}, //channel 0
     {1,0,0,0}, //channel 1
     {0,1,0,0}, //channel 2
@@ -60,13 +63,11 @@ int readMux(char file , int rank){
   };
 
   //loop through the 4 sig
-  for(int i = 0; i < 4; i ++){
-    digitalWrite(boards[file/2][i+1], muxChannel[(file % 2 == 0 ? 8: 0) + rank-1][i]);
+  for(int i = 0; i < 4; i ++)
+  {
+    digitalWrite(boards[file/2][i+1], muxChannel[file%2*8+rank-1][i]);
   }
 
   //read the value at the SIG pin
-  int val = analogRead(boards[file/2][0]);
-
-  //return the value
-  return val;
+  return analogRead(boards[file/2][0]);
 }
